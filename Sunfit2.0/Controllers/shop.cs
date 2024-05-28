425b05a878a4a3e576a6408eb9d56725de467c6f
Admin <Admin@DESKTOP-J78QH6O>
2024-05-28 23:39:17 +0500

    Add project files.

---
+++ Sunfit2.0/Controllers/shop.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sunfit2._0.Controllers
+{
+    public class shop : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
